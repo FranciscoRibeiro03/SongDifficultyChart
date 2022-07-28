2b7e433de0d218aa3e5eedaaeb3d58301f3c2a92
Riley Prince <rileyprince85@gmail.com>
2022-07-28 22:08:39 +0930

    pain

--- include/ModConfig.hpp
+++ include/ModConfig.hpp
@@ -5,12 +5,8 @@
 DECLARE_CONFIG(ModConfig,
 
     CONFIG_VALUE(Enabled, bool, "Enable Song Difficulty Chart", true);
-    CONFIG_VALUE(XPos, float, "Change X Position", 0.0f);
-    CONFIG_VALUE(YPos, float, "Change Y Position", -1.0f);
-    CONFIG_VALUE(ZPos, float, "Change Z Position", 3.0f);
-    CONFIG_VALUE(XRot, float, "Change X Rotation", 45.0f);
-    CONFIG_VALUE(YRot, float, "Change Y Rotation", 0.0f);
-    CONFIG_VALUE(ZRot, float, "Change Z Rotation", 0.0f);
+    CONFIG_VALUE(Positions, UnityEngine::Vector3, "Change Position", UnityEngine::Vector3(0.0f, -1.0f, 3.0f));
+    CONFIG_VALUE(Rotations, UnityEngine::Vector3, "Change Rotation", UnityEngine::Vector3(45.0f, 0.0f, 0.0f));
 
     CONFIG_VALUE(Frequency, float, "Change The Point Frequency", 1.0f);
 
@@ -21,12 +17,8 @@ DECLARE_CONFIG(ModConfig,
     CONFIG_INIT_FUNCTION(
 
         CONFIG_INIT_VALUE(Enabled);
-        CONFIG_INIT_VALUE(XPos);
-        CONFIG_INIT_VALUE(YPos);
-        CONFIG_INIT_VALUE(ZPos);
-        CONFIG_INIT_VALUE(XRot);
-        CONFIG_INIT_VALUE(YRot);
-        CONFIG_INIT_VALUE(ZRot);
+        CONFIG_INIT_VALUE(Positions);
+        CONFIG_INIT_VALUE(Rotations);
 
         CONFIG_INIT_VALUE(Frequency);
 
--- src/hooks/DifficultyChart.cpp
+++ src/hooks/DifficultyChart.cpp
@@ -8,25 +8,18 @@ using namespace QuestUI::BeatSaberUI;
 
 
 
-float spacing = 0.1f;
-
-
 MAKE_AUTO_HOOK_MATCH(DifficultyChart, &GlobalNamespace::GameplayCoreInstaller::InstallBindings, void, GlobalNamespace::GameplayCoreInstaller *self) {
 
     DifficultyChart(self);
 
     if (getModConfig().Enabled.GetValue() == true) {
-        UnityEngine::GameObject *screen = QuestUI::BeatSaberUI::CreateFloatingScreen(UnityEngine::Vector2(45.0f, 25.0f), UnityEngine::Vector3(getModConfig().XPos.GetValue(), getModConfig().YPos.GetValue(), getModConfig().ZPos.GetValue()),
-        UnityEngine::Vector3(getModConfig().XRot.GetValue(), getModConfig().YRot.GetValue(), getModConfig().ZRot.GetValue()), 0.0f, true, true, 5);
+        UnityEngine::GameObject *screen = QuestUI::BeatSaberUI::CreateFloatingScreen(UnityEngine::Vector2(45.0f, 25.0f), UnityEngine::Vector3(getModConfig().Positions),
+        UnityEngine::Vector3(getModConfig().Rotations), 0.0f, true, false);
     
-        QuestUI::BeatSaberUI::CreateText(screen->get_transform(), getModConfig().meme.GetValue(), UnityEngine::Vector2(25.0f, 0.0f));
+        QuestUI::BeatSaberUI::CreateText(screen->get_transform(), getModConfig().meme.GetValue(), UnityEngine::Vector2(20.0f, 0.0f));
 
-        Resources::FindObjectsOfTypeAll<MenuTransitionsHelper*>()[0]->RestartGame(nullptr);
     }
     else if (getModConfig().Enabled.GetValue() == false) {
-        Resources::FindObjectsOfTypeAll<MenuTransitionsHelper*>()[0]->RestartGame(nullptr);
+        
     }
-
-    
-
 }
